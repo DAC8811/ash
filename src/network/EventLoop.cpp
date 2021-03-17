501aac2007d9d91770506f343b497a4ad3bcaa66
DAC8811 <871230037@qq.com>
2021-03-17 19:38:47 +0800

    修改了EventLoop.cpp文件

--- src/network/EventLoop.cpp
+++ src/network/EventLoop.cpp
@@ -11,4 +11,28 @@ looping_(false),
 threadId_(muduo::CurrentThread::tid())
 {
     LOG_TRACE << "EventLoop created" << this << "in thread" << threadId_;
+    if(t_loopInThisThread){
+        LOG_FATAL << "Another EventLoop " << t_loopInThisThread << "exists in this thread " << threadId_;
+    }
+    else{
+        t_loopInThisThread = this;
+    }
+}
+
+EventLoop::~EventLoop(){
+    assert(!looping_);
+    t_loopInThisThread = NULL;
+}
+
+inline void EventLoop::assertInLoopThread(){
+    if(!isInLoopThread())
+        abortNotInLoopThread();
+}
+
+inline bool EventLoop::isInLoopThread() const{
+    return threadId_ == muduo::CurrentThread::tid();
+}
+
+EventLoop* EventLoop::getEventLoopOfCurrentThread(){
+    return t_loopInThisThread;
 }
